a329144a69f2da55791716b0a3d1769dadf71ff3
kalkidan <kalkidanteshale6@gmail.com>
2025-09-14 08:42:56 +0300

    Changing projected saving to a floating point number

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -5,7 +5,7 @@ monthly_savings = int(income) - int(expenses)
 yearly_savings = monthly_savings * 12
 rate = 0.05
 
-projected_savings = yearly_savings + (yearly_savings * rate)
+projected_savings = float(yearly_savings + (yearly_savings * rate))
 
 print("Your monthly savings are $", monthly_savings)
 print("Projected savings after one year, with interest, is: $", projected_savings)
