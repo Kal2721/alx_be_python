1dc96942198284d4e03bae52aa71588049ca9dd4
kalkidan <kalkidanteshale6@gmail.com>
2025-09-14 07:53:49 +0300

    Executing basic operations using python

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,6 @@
+number1 = 10
+number2 = 5
+
+print("Addition of", number1, "and",  number2, "is", number1 + number2 )  # Addition
+print("Subtraction of", number1, "and", number2, "is", number1 - number2)  # Subtraction 
+print("Multiplication of", number1, "and", number2, "is", number1 * number2)  # Multiplication
