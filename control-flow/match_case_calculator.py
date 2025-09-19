ae500ace23a6a95a41cb7709e4be9df2f00f545c
kalkidan <kalkidanteshale6@gmail.com>
2025-09-19 19:18:23 +0300

    Calculating input numbers according to the users choise using Match Case

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,18 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+operation = input("Choose the operation (+, -, *, /): ")
+
+match operation:
+    case "+":
+        print(num1 + num2)
+    case "-":
+        print(num1 - num2)
+    case "*":
+        print(num1 * num2)
+    case "/":
+        if num2 == 0:
+            print("Invalid operation")
+        else:
+            print(num1 / num2)
+    case _:
+        print("No number provided")
