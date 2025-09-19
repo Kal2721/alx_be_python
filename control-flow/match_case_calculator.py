89a2f9eb14668c070e2eb2922364068542d454f7
kalkidan <kalkidanteshale6@gmail.com>
2025-09-19 19:22:09 +0300

    Calculating input numbers according to the users choise using Match Case

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -4,11 +4,11 @@ operation = input("Choose the operation (+, -, *, /): ")
 
 match operation:
     case "+":
-        result = num1 + num2)
+        result = num1 + num2
     case "-":
-        result = num1 - num2)
+        result = num1 - num2
     case "*":
-        result = num1 * num2)
+        result = num1 * num2
     case "/":
         if num2 == 0:
             print("Invalid operation")
