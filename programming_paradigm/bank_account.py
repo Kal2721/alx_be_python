c9632c6d571121da8e53a11fff5158d16d36f9c3
kalkidan <kalkidanteshale6@gmail.com>
2025-10-05 07:01:28 +0300

    Creating a Simple Bank Account Class

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -20,4 +20,4 @@ class BankAccount:
             print("Withdrawal amount must be positive.")
 
     def display_balance(self):
-        return (f"Current balance: {self.account_balance}")
+        return (f"Current Balance: {self.account_balance}")
